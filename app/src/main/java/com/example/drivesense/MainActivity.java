b679c9a709c178ee0c9cf3d841a993b4edbabe46
Allis <allison261005@gmail.com>
2026-05-17 02:21:38 -0600

    refactor: configurar MainActivity y recursos globales para soporte de fragmentos
    - Se modifica MainActivity para gestionar el ciclo de vida de los nuevos componentes.
    - Se actualizan strings.xml y themes.xml con los estilos visuales de la aplicación.
    - Ajustes en el AndroidManifest para la inicialización correcta del proyecto.

--- app/src/main/java/com/example/drivesense/MainActivity.java
+++ app/src/main/java/com/example/drivesense/MainActivity.java
@@ -7,18 +7,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.drivesense.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        if (savedInstanceState == null) {
+            replaceFragment(new EstadisticasFragment());
+        }
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.buscar) {
+                replaceFragment(new BuscarRutaFragment());
+            } else if (id == R.id.registro) {
+                replaceFragment(new RegistroViajesFragment());
+            } else if (id == R.id.estadisticas) {
+                replaceFragment(new EstadisticasFragment());
+            } else if (id == R.id.perfil) {
+                replaceFragment(new PerfilFragment());
+            }
+
+            return true;
         });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
